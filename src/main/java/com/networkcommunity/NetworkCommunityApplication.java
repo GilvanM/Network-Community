42b4dbfc87b820425ca21312ce6a03c511e65fb9
Gilvan Menezes <gilvan.tude@hotmail.com>
2026-06-24 21:15:38 -0300

    feat: initial spring boot setup

---
+++ src/main/java/com/networkcommunity/NetworkCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.networkcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/networkcommunity/NetworkCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networkcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
