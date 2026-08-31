a16f54baa63b34fbd80a6898eb6d3108759197c4
Gilvan Menezes <gilvan.tude@hotmail.com>
2026-08-31 15:04:14 -0300

    Improve UI/UX with Bootstrap

--- src/main/java/com/networkcommunity/controller/HomeController.java
+++ src/main/java/com/networkcommunity/controller/HomeController.java
@@ -1,34 +1,13 @@
 package com.networkcommunity.controller;
 
-import com.networkcommunity.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-
 
 @Controller
 public class HomeController {
 
-    private final UserService userService;
-
-    public HomeController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping("/")
-    public String home(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size,
-            Model model) {
-
-        Pageable pageable = PageRequest.of(page, size);
-
-        model.addAttribute("users", userService.findAll(pageable).getContent());
-
-        return "home";
+    public String home() {
+        return "redirect:/posts";
     }
 }
